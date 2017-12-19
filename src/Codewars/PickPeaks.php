4cf43072f9c91ae8eed9da14026d65dd8de8bb71
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-19 19:57:35 +0700

    Update pick peaks

--- src/Codewars/PickPeaks.php
+++ src/Codewars/PickPeaks.php
@@ -1,10 +1,28 @@
 <?php
 
-namespace Codewars;
-
 /**
  *
  * @author Ammar Faizi <ammarfaizi2@gmail.com>
  * @license MIT
- * @link 
+ * @link https://www.codewars.com/kata/5279f6fe5ab7f447890006a7/train/php
  */
+function pickPeaks(array $arr)
+{
+	$peaks = ['pos' => [], 'peaks' => []];
+    $isPeaked = 0;
+    foreach ($arr as $key => $val) {
+    	if ($isPeaked) {
+    		$isPeaked = 0;
+    		continue;
+    	} else {
+    		if (isset($arr[$key+1])) {
+	    		if ($arr[$key+1] <= $val and !in_array($val, $peaks['peaks'])) {
+	    			$peaks['pos'][] = $key;
+	    			$peaks['peaks'][] = $val;
+	    			$isPeaked = 1;
+	    		}
+	    	}
+    	}
+    }
+    return $peaks;
+}
--- tests/Test.php
+++ tests/Test.php
@@ -9,5 +9,6 @@ class PickPeaksTest extends TestCase
     public function testItFindsPeaks()
     {
         $this->assertEquals(['pos' => [3,7], 'peaks' => [6,3]], pickPeaks([1,2,3,6,4,1,2,3,2,1]));
+        $this->assertEquals(['pos' => [1], 'peaks' => [2]], pickPeaks([1, 2, 2, 2, 1]));
     }
 }
